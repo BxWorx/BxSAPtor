883e4c6147d3ece83aa6dec486ba511cfa7faf90
Derrick Bingham <dbcm@mweb.co.za>
2017-07-11 11:58:32 +0200

    Project BxSAP_Utilities: Added & Namespace fix

---
+++ BxSAP/BxSAP_Utilities/Code Respository/Controller/iController.vb
@@ -0,0 +1,16 @@
+﻿Imports	BxSAP_Utilities.Serialization
+Imports	BxSAP_Utilities.Convertors
+'••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+Namespace Controllers
+	Public Interface iController
+
+		#Region "Section: Factory"
+
+			Function CreateCloner()				As iClone
+			Function CreateSerialiser()		As iSerialiser
+
+		#End Region
+
+	End Interface
+
+End Namespace
