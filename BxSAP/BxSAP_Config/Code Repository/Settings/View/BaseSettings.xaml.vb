490e15e9b3fd7962b6b030698426533b2b4b5977
Derrick Bingham <dbcm@mweb.co.za>
2017-07-25 17:52:43 +0200

    Update

--- BxSAP/BxSAP_Config/Code Repository/Settings/View/BaseSettings.xaml.vb
+++ BxSAP/BxSAP_Config/Code Repository/Settings/View/BaseSettings.xaml.vb
@@ -1,3 +1,12 @@
 ﻿Public Class BaseSettings
+	Public Sub New()
+
+		' This call is required by the designer.
+		InitializeComponent()
+
+		' Add any initialization after the InitializeComponent() call.
+
+	End Sub
+
 
 End Class
