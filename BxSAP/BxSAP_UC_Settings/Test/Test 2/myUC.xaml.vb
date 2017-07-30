8c45f2a18ac6bbae076c1da8758dda47df565061
Derrick Bingham <dbcm@mweb.co.za>
2017-07-30 16:30:48 +0200

    update

---
+++ BxSAP/BxSAP_UC_Settings/Test/Test 2/myUC.xaml.vb
@@ -0,0 +1,15 @@
+﻿'••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+Namespace	myUC
+
+	Public Class myUC
+
+			Protected ReadOnly Property Model() As myUCVM
+				Get
+					Return DirectCast(Resources("VM"), myUCVM)
+				End Get
+			End Property
+
+
+	End Class
+
+End Namespace
---
+++ BxSAP/BxSAP_UC_Settings/Test/Test 2/myUCVM.vb
@@ -0,0 +1,34 @@
+﻿Imports BxSAP_UC_Settings.Helpers
+'••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+Namespace myUC
+
+	Public Class myUCVM
+
+		Private co_SaveCmd	As RelayCommand
+
+
+		Public ReadOnly Property SaveCommand()	As RelayCommand
+			Get
+				Return	New RelayCommand(AddressOf Save)
+			End Get
+		End Property
+
+
+		Public Property Autosave()	As Boolean
+
+
+
+
+		Public Sub New()
+		End Sub
+
+
+
+		Private Sub Save()
+			Dim x = 1
+		End Sub
+
+
+	End Class
+
+End Namespace
