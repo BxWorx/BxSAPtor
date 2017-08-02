c9dc49772fcad6d49baa1dc342dc9f7885094aec
Derrick Bingham <dbcm@mweb.co.za>
2017-08-02 19:00:55 +0200

    update v02

---
+++ BxSAP/BxSAPtor_V02/Application.xaml.vb
@@ -0,0 +1,27 @@
+﻿Class Application
+
+	' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+	' can be handled in this file.
+
+
+	'	Protected Overrides Sub OnStartup(e As StartupEventArgs)
+
+	'		MyBase.OnStartup(e)
+
+	'		Dim app			As New ApplicationView()
+	'		Dim context	As New ApplicationViewModel()
+
+	'		app.DataContext = context
+	'		app.Show()
+
+	'End Sub
+
+	Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+
+		Dim lo_AppVM As New AppVM
+
+		Dim x = 1
+	End Sub
+
+
+End Class
---
+++ BxSAP/BxSAPtor_V02/Source Repository/Helpers/ViewModelBase.vb
@@ -0,0 +1,42 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+'••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+Namespace Helpers
+
+	Public MustInherit Class ViewModelBase
+														Implements	INotifyPropertyChanged
+
+		#Region "Methods: Protected"
+
+			'¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			Protected Function SetProperty(Of T)(											ByRef			_storage				As T	,
+																																					_value					As T	, 
+																						<CallerMemberName>	Optional	_propertyName		As String = Nothing) As Boolean
+
+				If EqualityComparer(Of T).[Default].Equals(_storage, _value)	Then	Return	False
+				'..................................................
+				_storage = _value
+				RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(_propertyName))
+				'Me.NotifyPropertyChanged(_propertyName)
+				Return True
+
+			End Function
+
+		#End Region
+		'¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+		#Region "Events: "
+
+			'¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			Friend Event PropertyChanged	As PropertyChangedEventHandler _
+																				Implements INotifyPropertyChanged.PropertyChanged
+
+			''¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+			'Private Sub NotifyPropertyChanged(ByVal propertyName As String)
+			'	RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+			'End Sub
+
+		#End Region
+
+	End Class
+
+End Namespace
